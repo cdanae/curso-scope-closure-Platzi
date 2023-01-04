07f672e2188f5b0aae94be4567f1c6be75c6ac7b
Carolina Danae Rodriguez Jimenez <c_danae@hotmail.com>
2023-01-04 13:12:11 -0600

    debug

---
+++ src/debug/index.js
@@ -0,0 +1,11 @@
+var a = 'Hola';
+
+function hello() {
+    let b = 'Hola mundo';
+    const c = 'Hola mundo!!!'
+    debugger;
+}
+
+hello();
+
+/* debugger sirve para detener la ejecución del programa, solo funciona en herramientas de desarrollo en navegador, aqui se muestra el tipo de scope de cada variable */
